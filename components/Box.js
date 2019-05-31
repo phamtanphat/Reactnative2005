df3f72261a474c4d7c5bc60c374a6d55fc7d0204
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-05-31 19:10:42 +0700

    a

--- components/Box.js
+++ components/Box.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react'
 import { Text, View , TouchableOpacity } from 'react-native'
 
 export default class Box extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value : 1
+        }
+    }
     render() {
         return(
             <View style={{flex : 1 , flexDirection : 'column' , justifyContent : 'center' , alignContent : 'center'}}>
-                <Text style={{fontSize : 20 , color : 'red' , alignSelf :'center'}}>Value = 0</Text>
+                <Text style={{fontSize : 20 , color : 'red' , alignSelf :'center'}}>Value = {this.state.value}</Text>
                 <View style={{flexDirection : 'row' }}>
                     <TouchableOpacity
                         style={{backgroundColor : 'green' , padding : 10 , borderRadius : 5}}
+                        onPress={() => this.setState({value : this.state.value + 1 })}
                     >
                         <Text style={{color : 'white' , fontSize : 20}}>Increase</Text>
                     </TouchableOpacity>
