32c276b8f93a533657e597faaf5aa245f1cd111a
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-06-07 19:25:04 +0700

    a

--- components/Form.js
+++ components/Form.js
@@ -1,45 +1,78 @@
 import React, { PureComponent } from 'react'
-import { Text, View , TouchableOpacity , TextInput , Dimensions} from 'react-native'
+import { Text, View, TouchableOpacity, TextInput, Dimensions } from 'react-native'
 
 const DeviceWidth = Dimensions.get("window").width
 export default class Form extends PureComponent {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state= {
-            txten : '',
-            txtvn : ''
+        this.state = {
+            txten: '',
+            txtvn: ''
         }
+
     }
-    render() {
-        return (
-            <View style={{flex : 1 ,flexDirection : 'column' , justifyContent : 'center' , alignItems : 'center'}}>
-                <View>
-                    <View style={{flexDirection : 'column' , backgroundColor : 'gainsboro' , padding : 10 , justifyContent : 'center' ,  alignItems : 'center'  }}>
-                        <TextInput 
-                            style={{height: DeviceWidth  * 0.15 , width : DeviceWidth * 0.7, backgroundColor: 'white', borderRadius :2 , paddingLeft : DeviceWidth * 0.05 , marginBottom : DeviceWidth * 0.03  , fontSize : 20}}
+    getForm() {
+        if (this.props.shouldShowForm) {
+            return (
+                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                    <View style={{ flexDirection: 'column', backgroundColor: 'gainsboro', padding: 10, justifyContent: 'center', alignItems: 'center' }}>
+                        <TextInput
+                            style={{ height: DeviceWidth * 0.15, width: DeviceWidth * 0.7, backgroundColor: 'white', borderRadius: 2, paddingLeft: DeviceWidth * 0.05, marginBottom: DeviceWidth * 0.03, fontSize: 20 }}
                             placeholder="English"
                             value={this.state.txten}
-                            onChangeText={(text) => this.setState({txten : text})}/>
-                        <TextInput 
-                            style={{height: DeviceWidth  * 0.15 , width : DeviceWidth * 0.7, backgroundColor: 'white', borderRadius :2 , paddingLeft : DeviceWidth * 0.05 , fontSize : 20}}
+                            onChangeText={(text) => this.setState({ txten: text })} />
+                        <TextInput
+                            style={{ height: DeviceWidth * 0.15, width: DeviceWidth * 0.7, backgroundColor: 'white', borderRadius: 2, paddingLeft: DeviceWidth * 0.05, fontSize: 20 }}
                             placeholder="Vietnamese"
                             value={this.state.txtvn}
-                            onChangeText={(text) => this.setState({txtvn : text})}/>
-                        </View>
+                            onChangeText={(text) => this.setState({ txtvn: text })} />
+                    </View>
 
-                    <View style={{flexDirection : "row" , marginTop : DeviceWidth * 0.01 , alignItems : "center"}}>
+                    <View style={{ flexDirection: "row", marginTop: DeviceWidth * 0.01, alignItems: "center" }}>
                         <TouchableOpacity
-                            style={{backgroundColor : "#218838" , padding : 10 , borderRadius : 8 , marginRight : DeviceWidth * 0.03}}
+                            style={{ backgroundColor: "#218838", padding: 10, borderRadius: 8, marginRight: DeviceWidth * 0.03 }}
+                            onPress={() => {
+                                if (this.state.txten.trim().length > 0 && this.state.txtvn.trim().length > 0) {
+                                    const word = {
+                                        id: Math.random(),
+                                        en: this.state.txten,
+                                        vn: this.state.txtvn,
+                                        isMemorized: false
+                                    }
+                                    const newWords = Object.assign([], this.state.words)
+                                    newWords.unshift(word)
+                                    this.setState({ words: newWords })
+                                } else {
+                                    alert("Truyen thong tin cho tu vung!!")
+                                }
+                            }}
                         >
-                            <Text style={{fontSize : DeviceWidth * 0.08 , color : 'white' }}>Add word</Text>
+                            <Text style={{ fontSize: DeviceWidth * 0.08, color: 'white' }}>Add word</Text>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            style={{backgroundColor : "#C82333", padding : 10 , borderRadius : 8}}
+                            style={{ backgroundColor: "#C82333", padding: 10, borderRadius: 8 }}
+                            onPress={() => this.setState({ shouldShowForm: false })}
                         >
-                            <Text style={{fontSize : DeviceWidth * 0.08 , color : 'white'}}>Cancel</Text>
+                            <Text style={{ fontSize: DeviceWidth * 0.08, color: 'white' }}>Cancel</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
+            )
+        }
+        return(
+        <TouchableOpacity
+            style={{ backgroundColor: "#218838", padding: 10, borderRadius: 8, width: DeviceWidth * 0.7, alignSelf: 'center', marginTop: DeviceWidth * 0.05 }}
+            onPress={() => this.setState({ shouldShowForm: true })}
+        >
+            <Text style={{ fontSize: DeviceWidth * 0.08, color: 'white', textAlign: 'center' }}>+</Text>
+
+        </TouchableOpacity>)
+    }
+
+    render() {
+        return (
+            <View>
+                {this.getForm()}
             </View>
         )
     }
--- components/List.js
+++ components/List.js
@@ -5,6 +5,8 @@ import { Text, View, ScrollView, Dimensions, TouchableOpacity, TextInput } from 
 import { Dropdown } from 'react-native-material-dropdown';
 import Word from './Word';
 import Fillter from './Fillter';
+import Form from './Form';
+
 
 
 const DeviceWidth = Dimensions.get('window').width
@@ -33,76 +35,6 @@ export default class List extends Component {
             shouldShowForm: false
         }
     }
-    getForm() {
-        if (this.state.shouldShowForm) {
-            return (
-                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                    <View style={{ flexDirection: 'column', backgroundColor: 'gainsboro', padding: 10, justifyContent: 'center', alignItems: 'center' }}>
-                        <TextInput
-                            style={{ height: DeviceWidth * 0.15, width: DeviceWidth * 0.7, backgroundColor: 'white', borderRadius: 2, paddingLeft: DeviceWidth * 0.05, marginBottom: DeviceWidth * 0.03, fontSize: 20 }}
-                            placeholder="English"
-                            value={this.state.txten}
-                            onChangeText={(text) => this.setState({ txten: text })} />
-                        <TextInput
-                            style={{ height: DeviceWidth * 0.15, width: DeviceWidth * 0.7, backgroundColor: 'white', borderRadius: 2, paddingLeft: DeviceWidth * 0.05, fontSize: 20 }}
-                            placeholder="Vietnamese"
-                            value={this.state.txtvn}
-                            onChangeText={(text) => this.setState({ txtvn: text })} />
-                    </View>
-
-                    <View style={{ flexDirection: "row", marginTop: DeviceWidth * 0.01, alignItems: "center" }}>
-                        <TouchableOpacity
-                            style={{ backgroundColor: "#218838", padding: 10, borderRadius: 8, marginRight: DeviceWidth * 0.03 }}
-                            onPress={() => {
-                                if (this.state.txten.trim().length > 0 && this.state.txtvn.trim().length > 0) {
-                                    const word = {
-                                        id: Math.random(),
-                                        en: this.state.txten,
-                                        vn: this.state.txtvn,
-                                        isMemorized: false
-                                    }
-                                    const newWords = Object.assign([], this.state.words)
-                                    newWords.unshift(word)
-                                    this.setState({ words: newWords })
-                                } else {
-                                    alert("Truyen thong tin cho tu vung!!")
-                                }
-                            }}
-                        >
-                            <Text style={{ fontSize: DeviceWidth * 0.08, color: 'white' }}>Add word</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={{ backgroundColor: "#C82333", padding: 10, borderRadius: 8 }}
-                            onPress={() => this.setState({ shouldShowForm: false })}
-                        >
-                            <Text style={{ fontSize: DeviceWidth * 0.08, color: 'white' }}>Cancel</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            )
-        }
-        return (
-            <TouchableOpacity
-                style={{ backgroundColor: "#218838", padding: 10, borderRadius: 8, width: DeviceWidth * 0.7, alignSelf: 'center', marginTop: DeviceWidth * 0.05 }}
-                onPress={() => this.setState({ shouldShowForm: true })}
-            >
-                <Text style={{ fontSize: DeviceWidth * 0.08, color: 'white', textAlign: 'center' }}>+</Text>
-            </TouchableOpacity>
-        )
-
-    }
-    getFilterForm() {
-        return (
-            <Dropdown
-                containerStyle={{ width: DeviceWidth * 0.95, height: DeviceWidth * 0.1, borderRadius: 5, borderWidth: 1, paddingLeft: DeviceWidth * 0.02, alignSelf: 'center', marginVertical: DeviceWidth * 0.05 }}
-                inputContainerStyle={{ borderBottomColor: 'transparent' }}
-                dropdownOffset={{ top: DeviceWidth * 0.01, left: 0 }}
-                data={this.state.fillterMode}
-                value={this.state.fillterPick}
-                onChangeText={text => this.setState({ fillterPick: text })}
-            />
-        )
-    }
     get Fitered(){
         return this.state.words.filter(w => {
             if (this.state.fillterPick === 'Show_ALL') return true
@@ -116,7 +48,7 @@ export default class List extends Component {
             <View style={{ flex: 1 }}>
                 <ScrollView>
                     <View style={{ flex: 1 }}>
-                        {this.getForm()}
+                        <Form shouldShowForm={this.state.shouldShowForm}/>
                         <Fillter/>
                         {this.Fitered.map(word => <Word word={word} key={word.id}/>)}
                     </View>
