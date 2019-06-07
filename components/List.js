b10c1ff3b2e46912b06e345f5af64a0329e610f7
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-06-07 17:57:29 +0700

    a

--- components/List.js
+++ components/List.js
@@ -20,11 +20,14 @@ export default class List extends Component {
                 { id: "a6", en: "Six", vn: "Sau", isMemorized: false },
             ],
             txten: '',
-            txtvn: ''
+            txtvn: '',
+            shouldShowForm : false
         }
     }
     getWordItem(word) {
+
         return (
+
             <View
                 key={word.id}
                 style={{ flex: 1, flexDirection: 'column', height: DeviceHeight * 0.2, margin: 10, backgroundColor: 'gainsboro', padding: 10, borderRadius: 5 }}>
@@ -66,52 +69,60 @@ export default class List extends Component {
         )
     }
     getForm() {
-        return (
-            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                <View style={{ flexDirection: 'column', backgroundColor: 'gainsboro', padding: 10, justifyContent: 'center', alignItems: 'center' }}>
-                    <TextInput
-                        style={{ height: DeviceWidth * 0.15, width: DeviceWidth * 0.7, backgroundColor: 'white', borderRadius: 2, paddingLeft: DeviceWidth * 0.05, marginBottom: DeviceWidth * 0.03, fontSize: 20 }}
-                        placeholder="English"
-                        value={this.state.txten}
-                        onChangeText={(text) => this.setState({ txten: text })} />
-                    <TextInput
-                        style={{ height: DeviceWidth * 0.15, width: DeviceWidth * 0.7, backgroundColor: 'white', borderRadius: 2, paddingLeft: DeviceWidth * 0.05, fontSize: 20 }}
-                        placeholder="Vietnamese"
-                        value={this.state.txtvn}
-                        onChangeText={(text) => this.setState({ txtvn: text })} />
-                </View>
-
-                <View style={{ flexDirection: "row", marginTop: DeviceWidth * 0.01, alignItems: "center" }}>
-                    <TouchableOpacity
-                        style={{ backgroundColor: "#218838", padding: 10, borderRadius: 8, marginRight: DeviceWidth * 0.03 }}
-                        onPress={() => {
-                            if (this.state.txten.trim().length > 0 && this.state.txtvn.trim().length > 0) {
-                                const word = {
-                                    id: Math.random(),
-                                    en: this.state.txten,
-                                    vn: this.state.txtvn,
-                                    isMemorized: false
+        if(this.state.shouldShowForm){
+            return (
+                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                    <View style={{ flexDirection: 'column', backgroundColor: 'gainsboro', padding: 10, justifyContent: 'center', alignItems: 'center' }}>
+                        <TextInput
+                            style={{ height: DeviceWidth * 0.15, width: DeviceWidth * 0.7, backgroundColor: 'white', borderRadius: 2, paddingLeft: DeviceWidth * 0.05, marginBottom: DeviceWidth * 0.03, fontSize: 20 }}
+                            placeholder="English"
+                            value={this.state.txten}
+                            onChangeText={(text) => this.setState({ txten: text })} />
+                        <TextInput
+                            style={{ height: DeviceWidth * 0.15, width: DeviceWidth * 0.7, backgroundColor: 'white', borderRadius: 2, paddingLeft: DeviceWidth * 0.05, fontSize: 20 }}
+                            placeholder="Vietnamese"
+                            value={this.state.txtvn}
+                            onChangeText={(text) => this.setState({ txtvn: text })} />
+                    </View>
+    
+                    <View style={{ flexDirection: "row", marginTop: DeviceWidth * 0.01, alignItems: "center" }}>
+                        <TouchableOpacity
+                            style={{ backgroundColor: "#218838", padding: 10, borderRadius: 8, marginRight: DeviceWidth * 0.03 }}
+                            onPress={() => {
+                                if (this.state.txten.trim().length > 0 && this.state.txtvn.trim().length > 0) {
+                                    const word = {
+                                        id: Math.random(),
+                                        en: this.state.txten,
+                                        vn: this.state.txtvn,
+                                        isMemorized: false
+                                    }
+                                    const newWords = Object.assign([], this.state.words)
+                                    newWords.unshift(word)
+                                    this.setState({ words: newWords })
+                                } else {
+                                    alert("Truyen thong tin cho tu vung!!")
                                 }
-                                const newWords = Object.assign([], this.state.words)
-                                newWords.unshift(word)
-                                this.setState({ words: newWords })
-                            } else {
-                                alert("Truyen thong tin cho tu vung!!")
-                            }
-
-
-                        }}
-                    >
-                        <Text style={{ fontSize: DeviceWidth * 0.08, color: 'white' }}>Add word</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={{ backgroundColor: "#C82333", padding: 10, borderRadius: 8 }}
-                    >
-                        <Text style={{ fontSize: DeviceWidth * 0.08, color: 'white' }}>Cancel</Text>
-                    </TouchableOpacity>
+                            }}
+                        >
+                            <Text style={{ fontSize: DeviceWidth * 0.08, color: 'white' }}>Add word</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={{ backgroundColor: "#C82333", padding: 10, borderRadius: 8 }}
+                        >
+                            <Text style={{ fontSize: DeviceWidth * 0.08, color: 'white' }}>Cancel</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-            </View>
+            )
+        }
+        return (
+            <TouchableOpacity
+                    style={{backgroundColor : "#218838" , padding : 10 , borderRadius : 8 ,width : DeviceWidth * 0.7, alignSelf : 'center' , marginTop : DeviceWidth * 0.05}}
+                >
+                     <Text style={{fontSize : DeviceWidth * 0.08 , color : 'white' ,textAlign : 'center'}}>+</Text>
+            </TouchableOpacity>
         )
+       
     }
     render() {
         return (
