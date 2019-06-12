2a5fd88511af7d2665fdc8f4a7334dc14d19a02f
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-06-12 19:33:40 +0700

    a

--- batdongbo.js
+++ batdongbo.js
@@ -1,1 +1,10 @@
-https://api.openweathermap.org/data/2.5/find?q=hanoi&units=metric&appid=86183a23377ed034aef7aad102f43d64
+// https://api.openweathermap.org/data/2.5/find?q=hanoi&units=metric&appid=86183a23377ed034aef7aad102f43d64
+
+// const request = require('request');
+// const URL = `https://api.openweathermap.org/data/2.5/find?q=hanoi&units=metric&appid=86183a23377ed034aef7aad102f43d64`
+// request(URL , (error , response , body) => {
+//     console.log(body);
+// })
+
+// Truyen vao quoc gia nao thi fucntion return nhiet do hien tai quoc gia do
+// getTemp(Hanoi) => thoi tiet hanoi la bnhiu do , bao neu khong quoc gia hien tai
