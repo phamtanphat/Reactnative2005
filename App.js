6566876cb7c2192d20cff064b5fe55b378931348
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-06-19 17:56:49 +0700

    a

--- App.js
+++ App.js
@@ -12,12 +12,21 @@ import List from './components/List';
 import Box from './components/Box';
 import Form from './components/Form';
 import Fillter from './components/Fillter';
+import { createStore } from 'redux'
+
+const store = createStore((state = 0 , action) => {
+    if(action.type == "INCREASE") return state + action.count
+    return state
+})
+alert(store.getState())
+store.dispatch({type : "INCREASE" , count : 1})
+alert(store.getState())
 
 export default class App extends Component {
   render() {
     return (
       <View style={{flex : 1 }}>
-        <List/>
+        
       </View>
     );
   }
